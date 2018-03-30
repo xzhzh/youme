19bb89b02d38dc812a3cf4961b45cd3282fe22bb
xzhzh <you@example.com>
2018-03-30 17:53:54 +0800

    gg

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,9 @@
 $(function(){
 
-
-
+/*模态框*/
+$('.warp .header .menus li:nth-child(4) ').click(function( ) {
+	$('.model').show();
+});
 
 
 })
--- routes/index.js
+++ routes/index.js
@@ -17,7 +17,16 @@ router.get('/view', function(req, res, next) {
 router.get('/message', function(req, res, next) {
   res.render('message', { title: 'Express' });
 });
-router.get('/circle', function(req, res, next) {
+router.get('/circle', function(req, res, next) {  //圈圈说
   res.render('circle', { title: 'Express' });
+});
+router.get('/comment', function(req, res, next) {  //评论
+  res.render('comment', { title: 'Express' });
+});
+router.get('/search', function(req, res, next) {  //搜索
+  res.render('search', { title: 'Express' });
+});
+router.get('/login', function(req, res, next) {  //微博登陆
+  res.render('login', { title: 'Express' });
 });
 module.exports = router;
